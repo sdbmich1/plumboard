1a1355babf61d6df9dd3c9563c490d73da42185a
tigstep <tigstep@gmail.com>
2014-05-05 20:30:31 -0700

    fixed the user name to be buyer's name

--- app/models/saved_listing_observer.rb
+++ app/models/saved_listing_observer.rb
@@ -4,8 +4,9 @@ class SavedListingObserver < ActiveRecord::Observer
 
   def after_create model
     # send notice to recipient
-    UserMailer.delay.send_save_pixi(model)
-
+    if (!model.listing.nil?)
+      UserMailer.delay.send_save_pixi(model)
+    end
     # reset saved pixi status
     # SavedListing.update_status_by_user model.user_id, model.pixi_id, 'saved'
 
