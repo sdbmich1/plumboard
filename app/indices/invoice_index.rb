2619f37ffd3a7396037067abe1ab605de2e14674
Sean Brown <sdbmich@yahoo.com>
2014-05-08 17:12:44 -0700

    update deploy.rb to add migrations; remove invoice_index.rb to fix remove invoice issue; update schedule.rb

--- app/indices/invoice_index.rb
+++
@@ -1,9 +0,0 @@
-ThinkingSphinx::Index.define :invoice, :with => :active_record, :delta => true do
-  indexes :id, :sortable => true
-  indexes listing(:title), :as => :title, :sortable => true
-  indexes buyer(:last_name), :as => :buyer, :sortable => true
-  indexes seller(:last_name), :as => :seller, :sortable => true
-
-  has :buyer_id, :seller_id, :pixi_id, :amount, :status, :created_at, :updated_at
-end
-
