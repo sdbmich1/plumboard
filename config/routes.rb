83a5b41dae3659c82578098d1bb313df5fc681a5
tigstep <tigstep@gmail.com>
2014-06-15 15:14:23 -0700

    Created _pixi_post_report.html.erb and _pixi_post_pixter_report_list.html.erb to pixter_report assignment

--- config/routes.rb
+++ config/routes.rb
@@ -41,10 +41,7 @@ Plumboard::Application.routes.draw do
 
   resources :pixi_posts do
     collection do
-      get 'seller', 'pixter', :autocomplete_site_name, :autocomplete_user_first_name
-    end
-    collection do
-      get 'pixter_report'
+      get 'seller', 'pixter', 'pixter_report', :autocomplete_site_name, :autocomplete_user_first_name
     end
   end
 
