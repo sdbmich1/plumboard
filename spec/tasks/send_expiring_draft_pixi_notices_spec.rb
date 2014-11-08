028105198a684e76df0f147ac517949e18dc3bf7
Junwei Yu <junweiyu@berkeley.edu>
2014-11-08 03:35:13 -0800

    added send_expiring_draft_pixi_notices rspec test

---
+++ spec/tasks/send_expiring_draft_pixi_notices_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'rake'
+
+describe 'manage_server namespace rake task' do
+  describe 'manage_server:send_expiring_draft_pixi_notices' do
+
+    before do
+      load File.expand_path("../../../lib/tasks/manage_server.rake", __FILE__)
+      Rake::Task.define_task(:environment)
+    end
+
+    it "should send expiring draft pixi notice" do
+      TempListing.should_receive :soon_expiring_pixis
+      Rake::Task["manage_server:send_expiring_draft_pixi_notices"].invoke(7)
+    end
+  end
+end
