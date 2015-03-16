881cc24dca2fbe339b5d818524be7d54220a6f57
sdbmich <sdbmich@yahoo.com>
2015-03-16 16:25:16 -0700

    fixed pixi_asks migrate file to remove drop statement

--- db/migrate/20141121211107_create_pixi_asks.rb
+++ db/migrate/20141121211107_create_pixi_asks.rb
@@ -1,6 +1,5 @@
 class CreatePixiAsks < ActiveRecord::Migration
   def change
-  	drop_table :pixi_asks #take out before pushing
     create_table :pixi_asks do |t|
       t.integer :user_id
       t.string :pixi_id
