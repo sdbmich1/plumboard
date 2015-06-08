7fd01b082989eed7841e06deb8534bbe32eee1ed
MAHESH VASHISHTHA <vashishtha.mahesh@gmail.com>
2015-06-08 14:29:08 -0700

    working on num3 from assignment

--- app/models/org_type.rb
+++ app/models/org_type.rb
@@ -1,3 +1,22 @@
 class OrgType < ActiveRecord::Base
   attr_accessible :code, :description, :hide, :status
+  
+  validates_presence_of :code, :description, :hide, :status
+
+  #default scope
+
+  # return active types
+  def self.active
+      where(:status => 'active')                              
+  end
+  
+  # return all unhidden types
+  def self.unhidden 
+      active.where(:hide => 'no')
+  end
+
+  # titelize descr
+  def nice_descr
+      description.titleize rescue nil
+  end   
 end
