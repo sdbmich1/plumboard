20f5ff8651ebad40a846519c4796af600438d37b
sdbmich <sdbmich@yahoo.com>
2016-03-05 17:02:24 -0800

    removed balanced initializer and gem file entry

--- config/initializers/balanced.rb
+++
@@ -1,4 +0,0 @@
-  require 'balanced'
-  BAL_API_KEYS = YAML::load_file("#{Rails.root}/config/gateway.yml")[Rails.env]
-  BALANCED_API_KEY = BAL_API_KEYS['balanced']['api_key']
-  BALANCED_URI = BAL_API_KEYS['balanced']['uri']
