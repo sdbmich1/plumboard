eba3c64f08ae71a11603d0256dc4ea6412b1a747
sdbmich <sdbmich@yahoo.com>
2016-03-27 10:39:55 -0700

    remove threshold setting from env files

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -78,7 +78,7 @@ Plumboard::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # set paperclip aws settings
   PAPERCLIP_STORAGE_OPTIONS = {:storage => :s3, 
